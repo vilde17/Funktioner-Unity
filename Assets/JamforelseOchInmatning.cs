b0b28e3480dfbe569662b737fb88052960f71fd1
Fabian Andersson <fabian.andersson2@elev.ga.lbs.se>
2018-09-27 13:53:08 +0200

    Git hub unity

---
+++ Assets/JamforelseOchInmatning.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JamforelseOchInmatning : MonoBehaviour
+{
+    public int MyIntValue = 20;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log(MyIntValue = (MyIntValue + 2));
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log(MyIntValue = (MyIntValue / 2));
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log(MyIntValue);
+        }
+    }
+
+
+
+}
